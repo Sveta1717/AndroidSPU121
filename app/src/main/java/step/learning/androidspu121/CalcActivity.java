f3542f0a4299deada36dd653cf6c5519a6b6a571
Sveta1717 <akai.iti@gmail.com>
2023-10-17 20:04:49 +0300

    17.10.2023

--- app/src/main/java/step/learning/androidspu121/CalcActivity.java
+++ app/src/main/java/step/learning/androidspu121/CalcActivity.java
@@ -9,9 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class CalcActivity extends AppCompatActivity {
     private TextView tvExpression ;
     private TextView tvResult ;
+
+    private double firstNumber = 0;
+    private double secondNumber = 0;
+    private String operator = "";
 
 
     @Override
@@ -20,8 +25,9 @@ public class CalcActivity extends AppCompatActivity {
         setContentView( R.layout.activity_calc );
 
         tvExpression = findViewById( R.id.calc_tv_expression ) ;
+        tvExpression.setText( "" );
         tvResult = findViewById( R.id.calc_tv_result ) ;
-        clearClick( null ) ;
+        tvResult.setText( R.string.calc_btn_0 ) ;
 
         findViewById( R.id.calc_btn_c ).setOnClickListener( this::clearClick ) ;
         // Пройти циклом по ідентифікаторах calc_btn_[i], всім вказати один обробник
@@ -35,21 +41,112 @@ public class CalcActivity extends AppCompatActivity {
                             )
             ).setOnClickListener( this::digitClick );
         }
+        findViewById(R.id.calc_btn_plus).setOnClickListener(this::operatorClick);
+        findViewById(R.id.calc_btn_minus).setOnClickListener(this::operatorClick);
+        findViewById(R.id.calc_btn_multiplication).setOnClickListener(this::operatorClick);
+        findViewById(R.id.calc_btn_divide).setOnClickListener(this::operatorClick);
+
+
+        findViewById(R.id.calc_btn_c).setOnClickListener(this::clearClick);
+        findViewById(R.id.calc_btn_ce).setOnClickListener(this::clearEntryClick);
+        findViewById(R.id.calc_btn_square).setOnClickListener(this::squareClick);
+        findViewById(R.id.calc_btn_sqrt).setOnClickListener(this::sqrtClick);
+        findViewById(R.id.calc_btn_equal).setOnClickListener(this::equalClick);
     }
-    private void digitClick( View view ) {  // для цифрових кнопок
-        String result = tvResult.getText().toString();
-        if (result.equals( getString(R.string.calc_btn_0 ) ) ) {
-            result = ((Button) view).getText().toString();
+
+    private void digitClick( View view ) {
+        String str = tvResult.getText().toString() ;
+        if( str.equals( getString( R.string.calc_btn_0 ) ) ) {
+            str = "";
+        }
+        str += ( ( Button ) view ).getText();
+        tvResult.setText( str );
+    }
+    private void clearClick( View view ) {
+        tvResult.setText( R.string.calc_btn_0 ) ;
+    }
+    private void inverseClick( View view ) {
+        String str = tvResult.getText().toString() ;
+        double arg = getResult( str ) ;
+        if( arg == 0 ) {
+            tvResult.setText( R.string.calc_div_zero_message );
         }
         else {
-            result += ((Button) view).getText();
+            str = "1 / " + str + " =" ;
+            tvExpression.setText( str );
+            showResult( 1 / arg );
         }
-
-        tvResult.setText(result);
     }
-    private void clearClick( View view ) {   // [ C ]
-        tvExpression.setText( "" ) ;
-        tvResult.setText( R.string.calc_btn_0 ) ;
+    private void showResult( double res ) {
+        String str = String.valueOf( res );
+        if( str.length() > 10 ) {
+            str = str.substring( 0, 10 ) ;
+        }
+        str = str.replaceAll( "0", getString( R.string.calc_btn_0 ) ) ;
+        tvResult.setText( str );
+    }
+    private double getResult( String str ) {
+        str = str.replaceAll( getString( R.string.calc_btn_0 ), "0" );
+        str = str.replaceAll( getString( R.string.calc_btn_comma ), "." );
+        return Double.parseDouble( str );
+    }
+
+    private void operatorClick(View view) {
+        if (!tvResult.getText().toString().isEmpty()) {
+            firstNumber = getResult(tvResult.getText().toString());
+            operator = ((Button) view).getText().toString();
+            tvExpression.setText(tvResult.getText() + " " + operator);
+            tvResult.setText(getString(R.string.calc_btn_0));
+        }
+    }
+
+    private void equalClick(View view) {
+        if (!operator.isEmpty() && !tvResult.getText().toString().isEmpty()) {
+            secondNumber = getResult(tvResult.getText().toString());
+            double result = 0;
+
+            if (operator.equals("+")) {
+                result = firstNumber + secondNumber;
+            } else if (operator.equals("-")) {
+                result = firstNumber - secondNumber;
+            } else if (operator.equals("*")) {
+                result = firstNumber * secondNumber;
+            } else if (operator.equals("/")) {
+                if (secondNumber != 0) {
+                    result = firstNumber / secondNumber;
+                } else {
+                    tvResult.setText(R.string.calc_div_zero_message);
+                    return;
+                }
+            }
+
+            showResult(result);
+            operator = "";
+            firstNumber = result; // Оновити перше число з результатом
+            secondNumber = 0;
+        }
+    }
+    private void squareClick(View view) {
+        if (!tvResult.getText().toString().isEmpty()) {
+            double num = getResult(tvResult.getText().toString());
+            double result = num * num;
+            showResult(result);
+        }
+    }
+
+    private void sqrtClick(View view) {
+        if (!tvResult.getText().toString().isEmpty()) {
+            double num = getResult(tvResult.getText().toString());
+            if (num >= 0) {
+                double result = Math.sqrt(num);
+                showResult(result);
+            } else {
+                tvResult.setText(R.string.calc_invalid_input);
+            }
+        }
+    }
+   private void clearEntryClick(View view) {
+        tvResult.setText(R.string.calc_btn_0);
     }
     /*
     Зміна конфігурації
